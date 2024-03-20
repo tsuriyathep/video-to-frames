1d5cc0570a71c4fca122cc95312ef175e90377b9
Tony Suriyathep <tony@spinplaygames.com>
2024-03-20 09:26:48 -0700

    Using temp path instead of tmp.

--- predict.py
+++ predict.py
@@ -2,6 +2,7 @@ from cog import BasePredictor, Input, Path
 from typing import List
 import subprocess
 import os
+import tempfile
 
 class Predictor(BasePredictor):
     def predict(self,
@@ -10,15 +11,15 @@ class Predictor(BasePredictor):
                 extract_all_frames: bool = Input(description="Get every frame of the video. Ignores fps. Slow for large videos.", default=False),
     ) -> List[Path]:
         """Run ffmpeg to split the video into frames"""
-        os.makedirs("/tmp/frames", exist_ok=True)
+        temp_folder_path = tempfile.mkdtemp()
 
         if not extract_all_frames:
-            command = f"ffmpeg -i {video} -vf fps={fps} /tmp/frames/out%03d.png"
+            command = f"ffmpeg -i {video} -vf fps={fps} {temp_folder_path}/frames/out%03d.png"
         else:
-            command = f"ffmpeg -i {video} /tmp/frames/out%03d.png"
+            command = f"ffmpeg -i {video} {temp_folder_path}/frames/out%03d.png"
 
         subprocess.run(command, shell=True, check=True)
-        frame_files = sorted(os.listdir("/tmp/frames"))
-        frame_paths = [Path(os.path.join("/tmp/frames", frame_file)) for frame_file in frame_files]
+        frame_files = sorted(os.listdir(f"{temp_folder_path}/frames"))
+        frame_paths = [Path(os.path.join(f"{temp_folder_path}/frames", frame_file)) for frame_file in frame_files]
 
         return frame_paths
